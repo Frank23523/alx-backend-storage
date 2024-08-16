10e54066c43f78e20072a86b87d9ff6d1d5b6c7e
Frank Anin <frankanin99@gmail.com>
2024-08-16 14:52:52 -0700

    nosql

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Log stats - new version"""
+
+from pymongo import MongoClient
+
+
+def log_stats():
+    """
+    Provides stats about Nginx logs stored in MongoDB
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+
+    # Total number of logs
+    total_logs = logs_collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    # Methods stats
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = logs_collection.count_documents({"method": method})
+        print(f"    method {method}: {count}")
+
+    # Number of status check
+    status_check = logs_collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+    })
+    print(f"{status_check} status check")
+
+    # Top 10 IPs
+    print("IPs:")
+    pipeline = [
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ]
+    top_ips = logs_collection.aggregate(pipeline)
+    for ip_info in top_ips:
+        print(f"    {ip_info['_id']}: {ip_info['count']}")
+
+if __name__ == "__main__":
+    log_stats()
